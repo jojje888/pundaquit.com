8af04fa21b27d294600ac27f2ae4fd9584a8b419
George <george.jonsson@gmail.com>
2026-01-07 10:48:11 +0800

    First submit

---
+++ js/common.js
@@ -0,0 +1,17 @@
+async function loadHtmlModule(elementId, filePath) {
+    const response = await fetch(filePath);
+    if (response.ok) {
+        const htmlText = await response.text();
+        let el = document.getElementById(elementId);
+        if (el) {
+            document.getElementById(elementId).insertAdjacentHTML('afterbegin', htmlText);
+        }
+        else {
+            //document.getElementById(elementId).innerHTML = 'Page not found.';
+            console.error('id "' + elementId + '" not found.');
+        }
+    }
+    else {
+        document.getElementById(elementId).innerHTML = 'Page not found.';
+    }
+}
